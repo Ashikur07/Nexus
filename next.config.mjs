ed42482883db49d31de2153d66824175454d7821
Ashikur07 <ashik.ict.iu@gmail.com>
2025-12-31 23:50:02 +0600

    update config file

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,13 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  /* config options here */
-};
+import withPWAInit from "@ducanh2912/next-pwa";
 
-export default nextConfig;
+const withPWA = withPWAInit({
+  dest: "public",
+  disable: process.env.NODE_ENV === "development", // ডেভেলপমেন্ট মোডে PWA বন্ধ থাকবে
+  // register: true,
+  // skipWaiting: true,
+});
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {};
+
+export default withPWA(nextConfig);
